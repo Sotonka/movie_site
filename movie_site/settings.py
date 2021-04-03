7149dab0116c755348232a0a4fc2eb3dda912cb6
Eugeny Alekseenko <puncong@mail.ru>
2021-04-03 14:32:36 +1000

    прикрутил html верстку, наследование шаблонов

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -14,7 +13,6 @@ SECRET_KEY = 'm9p=@u#q&ms^^4xk8vby!yt5j%7j7utq4*o64nzj3*+@33*o2&'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -59,7 +57,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'movie_site.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -69,7 +66,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -89,10 +85,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
@@ -103,11 +95,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_DIR = BASE_DIR / 'static'
+# список, из каких директорий нужно собирать статику
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Movie
 
-
+# КОНЕЦ 7 УРОКА ЬУЫИЗ ВЬЮ ЛИСТ ВЬЮ
+# 8 урок 6я минута перенести стиль
+#
+#
+#
 class MoviesView(View):
     """Список фильмов"""
     def get(self, request):
@@ -14,4 +18,14 @@ class MoviesView(View):
         return render(
             request, 'movies/movie_list.html', {'movie_list': movies}
         )
+
+
+class MovieDetailView(View):
+    """Полное описание фильма"""
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(
+            request, 'movies/movie_detail.html', {'movie': movie}
+        )
+
 
