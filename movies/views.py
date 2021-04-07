16aedd63ed22c675dc4ec381c260751409276ac8
Eugeny Alekseenko <puncong@mail.ru>
2021-04-07 23:33:26 +1000

    ответы на отзывы + верстка

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
+
 from .models import Movie
 from .forms import ReviewForm
 
@@ -24,6 +25,8 @@ class AddReview(View):
         movie = Movie.objects.get(id=pk)
         if form.is_valid():
             form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
