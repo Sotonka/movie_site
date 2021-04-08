f3e11f2867c0ab1b5897824a2265dd0cc194bafa
Eugeny Alekseenko <puncong@mail.ru>
2021-04-08 12:09:44 +1000

    вывод фильмов в сайдбаре

--- movies/templatetags/movie_tag.py
+++ movies/templatetags/movie_tag.py
@@ -1,5 +1,5 @@
 from django import template
-from movies.models import Category
+from movies.models import Category, Movie
 
 
 register = template.Library()
@@ -7,4 +7,12 @@ register = template.Library()
 
 @register.simple_tag()
 def get_categories():
+    """Вывод всех категорий"""
     return Category.objects.all()
+
+
+@register.inclusion_tag('movies/tags/last_movie.html')
+def get_last_movies():
+    """Вывод всех категорий"""
+    movies = Movie.objects.order_by("id")[:5]
+    return {"last_movies": movies}
