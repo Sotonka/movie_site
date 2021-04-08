ca2148cfcedc8d0ea889edc437d0e0de3c4414fa
Eugeny Alekseenko <puncong@mail.ru>
2021-04-08 12:54:05 +1000

    возможность указать количество фильмов в сайдбаре

--- movies/templatetags/movie_tag.py
+++ movies/templatetags/movie_tag.py
@@ -12,7 +12,7 @@ def get_categories():
 
 
 @register.inclusion_tag('movies/tags/last_movie.html')
-def get_last_movies():
+def get_last_movies(count=5):
     """Вывод всех категорий"""
-    movies = Movie.objects.order_by("id")[:5]
+    movies = Movie.objects.order_by("id")[:count]
     return {"last_movies": movies}
