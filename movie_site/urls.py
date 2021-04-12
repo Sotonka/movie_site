1e144ae0c0e002d94893bd7e9a72aecff495f4ca
Eugeny Alekseenko <puncong@mail.ru>
2021-04-12 16:42:42 +1000

    мультиязычность, не работает

--- movie_site/urls.py
+++ movie_site/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -6,10 +7,14 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('pages', include('django.contrib.flatpages.urls')),
-    path('contact/', include("contact.urls")),
-    path('', include('movies.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+urlpatterns += i18n_patterns(
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('contact/', include("contact.urls")),
+    path("", include("movies.urls")),
+)
 
 # При включенном debug-режиме django будет раздавать файлы из директории media
 if settings.DEBUG:
--- movies/admin.py
+++ movies/admin.py
@@ -4,20 +4,23 @@ from django.utils.safestring import mark_safe
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating,\
     RatingStar, Reviews
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from modeltranslation.admin import TranslationAdmin
 
 
 class MovieAdminForm(forms.ModelForm):
     """Виджет CKEditor"""
-    description = forms.CharField(label="Описание",
+    description_ru = forms.CharField(label="Описание",
                                   widget=CKEditorUploadingWidget())
 
+    description_en = forms.CharField(label="Описание",
+                                  widget=CKEditorUploadingWidget())
     class Meta:
         model = Movie
         fields = '__all__'
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslationAdmin):
     """Категории"""
     list_display = ("id", "name", "url")
     list_display_links = ("name",)
@@ -42,7 +45,7 @@ class MovieShotsInline(admin.TabularInline):
 
 
 @admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
+class MovieAdmin(TranslationAdmin):
     """Фильмы"""
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
@@ -89,13 +92,13 @@ class ReviewAdmin(admin.ModelAdmin):
 
 
 @admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
+class GenreAdmin(TranslationAdmin):
     """Жанры"""
     list_display = ("name", "url")
 
 
 @admin.register(Actor)
-class ActorAdmin(admin.ModelAdmin):
+class ActorAdmin(TranslationAdmin):
     """Актеры"""
     list_display = ("name", "age", "get_image")
     readonly_fields = ("get_image",)
@@ -113,7 +116,7 @@ class RatingAdmin(admin.ModelAdmin):
 
 
 @admin.register(MovieShots)
-class MovieShotsAdmin(admin.ModelAdmin):
+class MovieShotsAdmin(TranslationAdmin):
     """Кадры из фильма"""
     list_display = ("title", "movie", "get_image")
     readonly_fields = ("get_image",)
